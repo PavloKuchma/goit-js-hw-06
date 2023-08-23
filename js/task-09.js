d75ce109a66c907fc43819dfe7f7ed2784eb815d
PavloKuchma <111251902+PavloKuchma@users.noreply.github.com>
2023-08-23 16:05:28 +0300

    update task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,4 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-console.log(getRandomHexColor());
+const bodyEl = document.querySelector("body");
+const changeColBtn = document.querySelector(".change-color");
+const hexText = document.querySelector(".color");
+changeColBtn.addEventListener("click", colorChange);
+function colorChange(event) {
+  const hexVal = getRandomHexColor();
+  bodyEl.style.backgroundColor = hexVal;
+  hexText.textContent = hexVal;
+}
